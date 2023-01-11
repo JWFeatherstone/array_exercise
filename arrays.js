6cc2e7165bac0be8dd1ff0c1ffffd4bed3199d27
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-01-11 14:38:08 -0700

    Apply different methods to each array.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 var stringArray = ["cat", "dog", "pig"];
 var numberArray = [4, 7, 9, 12];
 var booleanArray = [false, false, true, false]
+// This method will remove the last element of the string array and, because I'm using console.log, print that final element.
+console.log(stringArray.pop())
+// This method will print a new array beginning from the second index position of the number array (9).
+console.log(numberArray.slice(2))
+// This method will add two, new values to the end of the boolean array and then print out the resulting, modified array.
+booleanArray.push(true, true)
+console.log(booleanArray)
