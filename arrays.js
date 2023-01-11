936211f797bd8d35095e1b528ac95500cf2dda4c
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-01-11 14:47:46 -0700

    Add index position demonstrations.

--- arrays.js
+++ arrays.js
@@ -8,3 +8,6 @@ console.log(numberArray.slice(2))
 // This method will add two, new values to the end of the boolean array and then print out the resulting, modified array.
 booleanArray.push(true, true)
 console.log(booleanArray)
+
+// The second element in the string array is "pig"
+// Performing a slice on the number array beginning with the element in the 0 index position and ending with the element in the 2 index position will return an array of [4, 7, 9]
